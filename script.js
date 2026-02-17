047d5ba5ce289fe828cfc576928d68631a5293ea
theabhishekujjawal <143508360+theabhishekujjawal@users.noreply.github.com>
2026-02-17 14:01:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+window.addEventListener("scroll", function() {
+    document.querySelectorAll(".section").forEach(section => {
+        const position = section.getBoundingClientRect().top;
+        if (position < window.innerHeight - 100) {
+            section.style.opacity = 1;
+            section.style.transform = "translateY(0)";
+        }
+    });
+});
 
